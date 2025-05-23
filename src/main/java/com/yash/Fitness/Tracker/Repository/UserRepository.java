8f608b325275aa886dab5b2693782a703ad0846e
Yash Mendiratta <yashmendiratta760@gmail.com>
2025-05-24 02:04:44 +0530

    First Server Upload

---
+++ src/main/java/com/yash/Fitness/Tracker/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.yash.Fitness.Tracker.Repository;
+
+import com.yash.Fitness.Tracker.entity.UserEntity;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<UserEntity, ObjectId>
+{
+    UserEntity findByUserName(String username);
+    boolean existsByUserName(String userName);
+}
---
+++ src/main/java/com/yash/Fitness/Tracker/entity/UserEntity.java
@@ -0,0 +1,36 @@
+package com.yash.Fitness.Tracker.entity;
+
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "Users")
+@Getter
+@Setter
+public class UserEntity
+{
+    @Id
+    private ObjectId id;
+
+    @Indexed(unique = true)
+    @NonNull
+    private String userName;
+
+    @NonNull
+    private String password;
+
+    @NonNull
+    private String email;
+
+    @DBRef
+    private List<StepsEntity> stepsEntity = new ArrayList<>();
+}
