420240bd79213a15cecdb32b02e2ad81a8a85d5d
Yash Mendiratta <yashmendiratta760@gmail.com>
2025-06-16 20:03:33 +0530

    Weekly History

--- src/main/java/com/yash/Fitness/Tracker/controller/StepsController.java
+++ src/main/java/com/yash/Fitness/Tracker/controller/StepsController.java
@@ -11,11 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/steps")
